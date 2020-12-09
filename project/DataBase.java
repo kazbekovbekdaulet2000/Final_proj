bd2f9048c92b70d692acc9bc342f8342455cc51e
Bekdaulet <kazbekov.bekdaulet2000@gmail.com>
2020-12-10 05:01:53 +0600

    Serialization and deserialization

--- project/DataBase.java
+++ project/DataBase.java
@@ -3,9 +3,11 @@ package project;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Vector;
 
 import Users.User;
@@ -43,37 +45,95 @@ public class DataBase {
     }
     
     private void serializeNews() {
-    	//TODO
+    	try {
+            ObjectInputStream out = new ObjectInputStream(new FileInputStream("news.ser"));
+            news = (Vector<News>) out.readObject();//    ???
+            out.close();
+        }
+        catch (ClassNotFoundException e) {
+            System.out.println("Class not found");
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+        catch (IOException e) {
+            System.out.println("IO exception");
+        }
 	}
 
 	private void serializeOrders() {
-		// TODO Auto-generated method stub
-		
+		try {
+            ObjectInputStream out = new ObjectInputStream(new FileInputStream("orders.ser"));
+            orders = (Vector<Order>) out.readObject();//    ???
+            out.close();
+        }
+        catch (ClassNotFoundException e) {
+            System.out.println("Class not found");
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+        catch (IOException e) {
+            System.out.println("IO exception");
+        }
 	}
 
 	private void serializeFiles() {
-		// TODO Auto-generated method stub
-		
+		try {
+	        ObjectInputStream out = new ObjectInputStream(new FileInputStream("course_files.ser"));
+	        files = (Vector<Course_File>) out.readObject();//    ???
+	        out.close();
+	    }
+	    catch (ClassNotFoundException e) {
+	        System.out.println("Class not found");
+	    }
+	    catch (FileNotFoundException e) {
+	        System.out.println("File not found");
+	    }
+	    catch (IOException e) {
+	        System.out.println("IO exception");
+	    }
 	}
 
 	private void serializeMarks() {
-		// TODO Auto-generated method stub
-		
+		try {
+            ObjectInputStream out = new ObjectInputStream(new FileInputStream("marks.ser"));
+            marks = (Vector<Mark>) out.readObject();//    ???
+            out.close();
+        }
+        catch (ClassNotFoundException e) {
+            System.out.println("Class not found");
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+        catch (IOException e) {
+            System.out.println("IO exception");
+        }
 	}
 
 	private void serializeCourses() {
-		// TODO Auto-generated method stub
-		
+		try {
+            ObjectInputStream out = new ObjectInputStream(new FileInputStream("courses.ser"));
+            courses = (Vector<Course>) out.readObject();//    ???
+            out.close();
+        }
+        catch (ClassNotFoundException e) {
+            System.out.println("Class not found");
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+        catch (IOException e) {
+            System.out.println("IO exception");
+        }
 	}
 
 	private void serializeUsers() {
     	try {
             ObjectInputStream out = new ObjectInputStream(new FileInputStream("users.ser"));
-
-            users = (Vector<User>) out.readObject();
-
+            users = (Vector<User>) out.readObject();//    ???
             out.close();
-
         }
         catch (ClassNotFoundException e) {
             System.out.println("Class not found");
@@ -87,27 +147,121 @@ public class DataBase {
 	}
 
 	public void deserializeAll() {
-        //TODO
+		deserializeUsers();
+        deserializeCourses();
+        deserializeFiles();
+        deserializeMarks();
+        deserializeOrders();
+        deserializeNews();
     }
     
-    public void readDataBase(Course c) {
-    	try {
-    		FileReader fr = new FileReader("allData.txt");
-			BufferedReader br = new BufferedReader(fr);
-			String str = br.readLine();
-			
-			while(str != null) {
-				str = br.readLine();
-				System.out.println(str);
-			}
-			br.close();
-		} catch (IOException e) {
-			System.out.println("No such file founded");
-		}
+	private void deserializeUsers() {
+		try {
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("users.ser"));
+            out.writeObject(users);
+            out.flush();
+            out.close();
+        }
+        catch (FileNotFoundException e) {
+        	System.out.println("Class not found");
+        }
+        catch (IOException e) {
+        	System.out.println("IO Exception");
+        }
+	}
+	
+	private void deserializeCourses() {
+		try {
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("courses.ser"));
+            out.writeObject(courses);
+            out.flush();
+            out.close();
+        }
+        catch (FileNotFoundException e) {
+        	System.out.println("Class not found");
+        }
+        catch (IOException e) {
+        	System.out.println("IO Exception");
+        }
+	}
+	
+	private void deserializeFiles() {
+		try {
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("course_files.ser"));
+            out.writeObject(files);
+            out.flush();
+            out.close();
+        }
+        catch (FileNotFoundException e) {
+        	System.out.println("Class not found");
+        }
+        catch (IOException e) {
+        	System.out.println("IO Exception");
+        }
+	}
+	
+	private void deserializeMarks() {
+		try {
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("marks.ser"));
+            out.writeObject(marks);
+            out.flush();
+            out.close();
+        }
+        catch (FileNotFoundException e) {
+        	System.out.println("Class not found");
+        }
+        catch (IOException e) {
+        	System.out.println("IO Exception");
+        }
+	}
+	
+	private void deserializeOrders() {
+		try {
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("orders.ser"));
+            out.writeObject(orders);
+            out.flush();
+            out.close();
+        }
+        catch (FileNotFoundException e) {
+        	System.out.println("Class not found");
+        }
+        catch (IOException e) {
+        	System.out.println("IO Exception");
+        }
+		
+	}
+	
+	private void deserializeNews() {
+		try {
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("news.ser"));
+            out.writeObject(news);
+            out.flush();
+            out.close();
+        }
+        catch (FileNotFoundException e) {
+        	System.out.println("Class not found");
+        }
+        catch (IOException e) {
+        	System.out.println("IO Exception");
+        }
+	}
+
+
+    public void readDataBase(Course c) { //just an idea
+//    	try {
+//    		FileReader fr = new FileReader("allData.txt");
+//			BufferedReader br = new BufferedReader(fr);
+//			String str = br.readLine();
+//			
+//			while(str != null) {
+//				str = br.readLine();
+//				System.out.println(str);
+//			}
+//			br.close();
+//		} catch (IOException e) {
+//			System.out.println("No such file founded");
+//		}
     }
 
-	
-    
-    
     
 }
